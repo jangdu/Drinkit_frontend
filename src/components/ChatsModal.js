e84a9fbd1b07b1aa4c8dbf06e6e43cbaaa56c817
jangdu <jjd0324@gmail.com>
2023-09-01 12:43:01 +0900

    feat: socket

--- src/components/ChatsModal.js
+++ src/components/ChatsModal.js
@@ -1,59 +1,71 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Button from "./ui/Button";
+import { useAuthContext } from "../context/AuthContext";
 
 const ChatsModal = ({ clickedRoom, socket, setModalIsOpen }) => {
   const [messages, setMessages] = useState([]);
   const [messageInput, setMessageInput] = useState("");
+  const { user } = useAuthContext();
 
   useEffect(() => {
+    console.log("momomo");
+
     // 채팅방에 입장하기 위해 서버에 join 이벤트를 전송합니다.
-    console.log("aaaaa");
-    socket.emit("joinRoom", {
-      roomName: clickedRoom.name,
-      maxNumberOfPerson: clickedRoom.maxNumberOfPerson,
-      nickname: "사용자 닉네임", // 사용자의 닉네임을 여기에 설정해야 합니다.
-    });
+    console.log(clickedRoom);
+    socket.emit("joinRoom", clickedRoom);
+    socket.on("joinedRoom", handleNewMessage);
+    socket.on("userJoined", handleNewMessage);
+    socket.on("broadcastMessage", handleNewMessage);
 
-    // 서버로부터 새로운 메시지를 받았을 때 처리하는 이벤트 핸들러를 등록합니다.
-    socket.on("broadcastMessage", (data) => {
-      // 받은 메시지를 기존 메시지 목록에 추가합니다.
-      setMessages((prevMessages) => [...prevMessages, data]);
-    });
+    ///////////////////////////////////////////
+    const iceServers = [
+      { urls: "stun:stun.l.google.com:19302" }, // Google STUN 서버 주소
+    ];
 
-    // 컴포넌트 언마운트 시 이벤트 핸들러를 정리합니다.
+    const peerConnection = new RTCPeerConnection({ iceServers });
+
     return () => {
       socket.off("broadcastMessage");
+      socket.off("userJoined");
+      socket.off("joinedRoom");
     };
-  }, []);
+  }, [socket, clickedRoom]);
 
   const handleSendMessage = () => {
     if (messageInput.trim() !== "") {
       // 입력한 메시지를 서버로 전송합니다.
       socket.emit("sendChat", messageInput);
+      setMessages((prevMessages) => [...prevMessages, `me: ${messageInput}`]);
       setMessageInput("");
     }
   };
 
+  const handleNewMessage = (data) => {
+    setMessages((prevMessages) => [...prevMessages, `${data}`]);
+  };
+
   return (
-    <div>
+    <div style={{ height: "90%", overflowY: "auto" }}>
       <h2>{clickedRoom.name} 채팅방</h2>
-      <div style={{ maxHeight: "300px", overflowY: "auto" }}>
+      <div>
         {messages.map((message, index) => (
           <div key={index}>{message}</div>
         ))}
       </div>
-      <div>
+      <div className="absolute bottom-0">
         <input type="text" value={messageInput} onChange={(e) => setMessageInput(e.target.value)} placeholder="메시지를 입력하세요." />
         <Button text="전송" onClick={handleSendMessage} />
       </div>
-      <Button
-        text="나가기"
-        onClick={() => {
-          // 채팅방에서 나가기 위해 서버에 leave 이벤트를 전송합니다.
-          socket.emit("leaveRoom", clickedRoom.name);
-          setModalIsOpen(false);
-        }}
-      />
+      <div className="absolute right-3 top-3">
+        <Button
+          text="나가기"
+          onClick={() => {
+            // 채팅 나가기
+            socket.emit("leaveRoom", clickedRoom.name);
+            setModalIsOpen(false);
+          }}
+        />
+      </div>
     </div>
   );
 };
