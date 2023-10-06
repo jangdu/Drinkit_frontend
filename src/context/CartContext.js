804f842d865e9f3344b77e94327d1237d7943ac8
jangdu <98936510+jangdu@users.noreply.github.com>
2023-10-06 19:34:17 +0900

    docs : ContextAPI 주석 추가

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,5 +1,3 @@
-// cartContext.js
-
 import React, { useState, useContext } from "react";
 
 // 초기 장바구니 상태
@@ -13,46 +11,46 @@ const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState(initialCartItems);
   const [clickedPickupBtn, setClickedPickupBtn] = useState(false);
 
+  // 장바구니 추가
   const addToCart = async (newItem) => {
     // 중복된 productId를 가진 메뉴가 이미 장바구니에 있는지 체크
     const existingItemIndex = cartItems.findIndex((item) => item.productId === newItem.productId);
 
     if (existingItemIndex !== -1) {
-      // 이미 장바구니에 있는 경우 count만 업데이트
       const updatedCart = [...cartItems];
+      
       updatedCart[existingItemIndex].count += newItem.count;
+      
       setCartItems(updatedCart);
     } else {
-      // 장바구니에 없는 경우 새로 추가
       setCartItems([...cartItems, newItem]);
     }
   };
 
+  // 장바구니 내 상품 갯수 +1
   const increaseCount = (productId) => {
     const updatedCart = cartItems.map((item) => (item.productId === productId ? { ...item, count: item.count + 1 } : item));
     setCartItems(updatedCart);
   };
 
+  // 장바구니 내 상품 갯수 -1
   const decreaseCount = (productId) => {
     const updatedCart = cartItems.map((item) => (item.productId === productId && item.count > 1 ? { ...item, count: item.count - 1 } : item));
     setCartItems(updatedCart);
   };
 
+  // 장바구니 내 상품 삭제
   const removeFromCart = (index) => {
     const updatedCart = cartItems.filter((item) => item.productId !== index);
     setCartItems(updatedCart);
   };
 
-  const removeMyCart = () => {
-    setCartItems([]);
-  };
-
-  // 장바구니 내 메뉴 개수를 총합 계산하는 함수
+  // 장바구니 내 메뉴 개수 총합
   const getTotalCount = () => {
     return cartItems.reduce((total, item) => total + item.count, 0);
   };
 
-  // 장바구니 내 메뉴 가격의 총합 계산하는 함수
+  // 장바구니 내 메뉴 가격의 총합
   const getTotalPrice = () => {
     return cartItems.reduce((total, item) => total + item.price * item.count, 0).toLocaleString();
   };
